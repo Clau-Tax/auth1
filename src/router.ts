7e5fe3b6a1327cec29a9242d4a57fa3508c885de
Claudia Tax <claudiatax05@gmail.com>
2026-05-01 16:18:09 -0600

    primer commit

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import expres from 'express';
+import { login, register } from './auth/auth.controller.js';
+import { currentUser } from './users/user.controller.js';
+import { authMiddleware } from './middlewares/auth.middleware.js';
+
+const router = expres.Router();
+
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/user',authMiddleware,currentUser)
+
+export default router;
